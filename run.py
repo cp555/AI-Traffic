a10a76c1750b9961440bc52ebf8886c55f792d10
sh0829kk <381534335@qq.com>
2023-02-15 15:33:09 -0800

    save metrics to json file

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 Author: sh0829kk 381534335@qq.com
 Date: 2023-02-08 21:57:11
 LastEditors: sh0829kk 381534335@qq.com
-LastEditTime: 2023-02-15 15:12:43
+LastEditTime: 2023-02-15 15:31:56
 FilePath: /AI-Traffic/run.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -69,5 +69,7 @@ if __name__ == "__main__":
         step += 1
 
     print(metrics)
+    with open("metrics.json", "w") as outfile:
+        json.dump(metrics, outfile)
 
     traci.close(False)
