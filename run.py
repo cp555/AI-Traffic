b3a75a2e41d45600474523f3b046baf86a3d9355
sh0829kk <381534335@qq.com>
2023-02-15 15:12:01 -0800

    add metrics under run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 Author: sh0829kk 381534335@qq.com
 Date: 2023-02-08 21:57:11
 LastEditors: sh0829kk 381534335@qq.com
-LastEditTime: 2023-02-13 16:20:33
+LastEditTime: 2023-02-13 19:05:25
 FilePath: /AI-Traffic/run.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -91,7 +91,7 @@ if __name__ == "__main__":
         ## RUN Data Analysis 
         step += 1
     # print(metrics)
-    metrics = metrics.
+  
     # print('Timeloss',traci.vehicle.getTimeLoss(7))
 
 
