d87569618eecbc47a3018711e093e534901ee319
sh0829kk <381534335@qq.com>
2023-02-21 10:51:44 -0800

    change metrics data structure

--- analysis.py
+++ analysis.py
@@ -2,17 +2,32 @@
 Author: sh0829kk 381534335@qq.com
 Date: 2023-02-08 15:25:37
 LastEditors: sh0829kk 381534335@qq.com
-LastEditTime: 2023-02-13 15:15:20
+LastEditTime: 2023-02-15 15:59:15
 FilePath: /AI-Traffic/test.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
-#/usr/bin/python
+# /usr/bin/python
 
-def updateMetrics(conn,metrics,state,geometry):
-    for lane in geometry["LaneID"]:
-        metrics['WaitingTime'].append(conn.lane.getWaitingTime(lane))
-        metrics['CO2'].append(conn.lane.getCO2Emission(lane))
-            
-    for vehicle in state["vehicleID"]:
-        metrics['TimeLoss'].append(conn.vehicle.getTimeLoss(vehicle))
+
+def updateMetrics(step, conn, metrics, state, geometry, key):
+    if key == 'lane':
+        for lane in geometry["LaneID"]:
+            if lane not in metrics:
+                metrics[lane] = {'step': [], 'WaitingTime': [], 'CO2': []}
+            metrics[lane]['step'].append(step)
+            metrics[lane]['WaitingTime'].append(conn.lane.getWaitingTime(lane))
+            metrics[lane]['CO2'].append(conn.lane.getCO2Emission(lane))
+    if key == 'vehicle':
+        for vehicle in state["vehicleID"]:
+            if vehicle not in metrics:
+                metrics[vehicle] = {
+                    'step': [], 'TimeLoss': [], 'AccumulatedWaitingTime': []}
+            metrics[vehicle]['step'].append(step)
+            metrics[vehicle]['TimeLoss'].append(
+                conn.vehicle.getTimeLoss(vehicle))
+            metrics[vehicle]['AccumulatedWaitingTime'].append(conn.vehicle.getAccumulatedWaitingTime(vehicle))
     return metrics
+
+    # track time-step, vehicle-id, control-signal
+    # metrics-land & metrics-vehicle
+    #
--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 Author: sh0829kk 381534335@qq.com
 Date: 2023-02-08 21:57:11
 LastEditors: sh0829kk 381534335@qq.com
-LastEditTime: 2023-02-15 15:31:56
+LastEditTime: 2023-02-15 15:59:02
 FilePath: /AI-Traffic/run.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -12,21 +12,22 @@ from Network import Network
 from Controller import MaxPressureController
 from Controller import dqnController
 import traci
-import os, sys
+import os
+import sys
 import json
-from analysis import updateMetrics 
+from analysis import updateMetrics
 
 if __name__ == "__main__":
 
     controller_type = "max_pressure"
 
     # LOAD SUMO STUFF
-    cfgfilename = "test_1110.sumo.cfg" # sys.argv[1]
+    cfgfilename = "test_1110.sumo.cfg"  # sys.argv[1]
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    filepath = os.path.join(dir_path,"network",cfgfilename)
+    filepath = os.path.join(dir_path, "network", cfgfilename)
     sumoCmd = ["sumo", "-c", filepath]
-    #sumoCmd = ["sumo-gui", "-c", filepath]  # if you want to see the simulation
+    # sumoCmd = ["sumo-gui", "-c", filepath]  # if you want to see the simulation
 
     # initialize the network object and controller object
     tracilabel = "sim1"
@@ -35,41 +36,43 @@ if __name__ == "__main__":
 
     network = Network(filepath, conn)
     controller = "max_pressure"
-    if controller_type=="max_pressure":
+    if controller_type == "max_pressure":
         controller = MaxPressureController()
-    else :
+    else:
         controller = dqnController()
 
     step = 0
 
-    metrics = {'WaitingTime':[],'AccumulatedWaitinTime':[],'CO2':[],'TimeLoss':[]}
+    metrics_lane = {}
+    metrics_vehicle = {}
     while conn.simulation.getMinExpectedNumber() > 0:
         conn.simulationStep()
-        if step > 1 and step%30 == 0: 
+        if step > 1 and step % 30 == 0:
 
             # get current state
             state = network.getState(conn)
             geometry = network.getGeometry()
 
             # get maxpressure controller
-            control = controller.getController(geometry,state)
+            control = controller.getController(geometry, state)
 
             # update the state of the network
-            network.applyControl(control,conn)      
+            network.applyControl(control, conn)
 
-            print('vehicle',network.state["vehicleID"])
-            
-            #########write_state_to_file(state)   
-            metrics = updateMetrics(conn,metrics,state,geometry)
-           
+            # print('vehicle', network.state["vehicleID"])
 
-    
-  
-        ## RUN Data Analysis 
+            # write_state_to_file(state)
+            metrics_lane = updateMetrics(
+                step, conn, metrics_lane, state, geometry, key='lane')
+            metrics_vehicle = updateMetrics(
+                step, conn, metrics_vehicle, state, geometry, key='vehicle')
+
+        # RUN Data Analysis
         step += 1
 
-    print(metrics)
-    with open("metrics.json", "w") as outfile:
-        json.dump(metrics, outfile)
+    with open("metrics_lane.json", "w") as outfile:
+        json.dump(metrics_lane, outfile)
+    with open("metrics_vehicle.json", "w") as outfile:
+        json.dump(metrics_vehicle, outfile)
 
     traci.close(False)
